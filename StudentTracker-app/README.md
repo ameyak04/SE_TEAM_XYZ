13f19c83f9cd1099636350874cefcf4b85c7e272
ameyak04 <ameya.kanagalekar@gmail.com>
2024-05-03 00:23:41 +0530

    feat:added app

---
+++ StudentTracker-app/README.md
@@ -0,0 +1,11 @@
+## ⚙️ Detailed Functionality
+* Teachers and CRs can mark the attendance of courses currently teaching
+* Student can check their attendance subject wise and also get overall report by single click   
+* Students can get awesome resources like Computer Courses, Movies, Games, TV series Free
+* Admin Panel can add teachers,students and can generate specified date attendance in single click.
+ 
+## 🚀 Tech and Tools Used
+
+* Firebase Database
+* Android Studio
+* LottieFiles
---
+++ StudentTracker-app/app/src/main/java/com/example/attendo2/adminattendance.java
@@ -0,0 +1,117 @@
+package com.example.attendo2;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class adminattendance extends AppCompatActivity {
+    ListView listView;
+    String sid,teacher_id;
+
+
+    EditText date;
+    ArrayList Userlist = new ArrayList<>();
+    ArrayList Studentlist = new ArrayList<>();
+
+    DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference dbAttendance;
+    DatabaseReference dbStudent;
+    String required_date;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adminattendance);
+
+        listView = (ListView) findViewById(R.id.list);
+        date = (EditText) findViewById(R.id.date);
+        //sid = "mukesh";
+        teacher_id = "Mukesh";
+    }
+    public void viewList(View v) {
+
+        Userlist.clear();
+        dbStudent = ref.child("Student");
+        dbStudent.orderByChild("classes").equalTo("CSE").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot dsp : dataSnapshot.getChildren()) {
+                    Userlist.add(dsp.child("sid").getValue().toString());
+                }
+                display_list(Userlist);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Toast.makeText(getApplicationContext(), "something went wrong", Toast.LENGTH_LONG).show();
+            }
+
+        });
+    }
+
+    public void display_list(final ArrayList userlist) {
+
+        Studentlist.clear();
+        required_date = date.getText().toString();
+        dbAttendance = ref.child("attendance");
+        Studentlist.add("      SID       "+"Status" + "   period");
+        for (Object sid : userlist) {
+            dbAttendance.child(required_date).child("CSE").child("123").addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    for(DataSnapshot dsp : dataSnapshot.getChildren()) {
+                        String p1 = dsp.getValue().toString();
+                        Log.d("p1",p1 );
+                        Log.d("teacher",dataSnapshot.getKey().toString());
+                        if((p1.equals("{p1=A}"))||(p1.equals("{p1=P}"))){
+                            Studentlist.add(dsp.getKey().toString() + "            " + p1.substring(4,5) +"        "+"p1");
+                        }
+                    }
+                    list(Studentlist);
+
+                }
+
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    Toast.makeText(getApplicationContext(), "something went wrong", Toast.LENGTH_LONG).show();
+                }
+
+            });
+
+
+        }
+
+    }
+    public void list(ArrayList studentlist){
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, studentlist);
+        listView.setAdapter(adapter);
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
